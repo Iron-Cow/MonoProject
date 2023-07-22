e04df5b3c9c7e71b92b63c5dbbd3858c67466d5d
Iron-Cow <uangeji@gmail.com>
2023-07-22 17:30:18 +0300

    fixed tests

--- django_api/api/api/settings.py
+++ django_api/api/api/settings.py
@@ -91,12 +91,10 @@ WSGI_APPLICATION = "api.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", "github_actions"),
-        "HOST": os.getenv("DB_HOST", "127.0.0.1")
-        if not os.getenv("DEBUG")
-        else "localhost",
-        "USER": os.getenv("DB_USER", "postgres"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "postgres"),
+        "NAME": os.getenv("DB_NAME"),
+        "HOST": os.getenv("DB_HOST") if not os.getenv("DEBUG") else "localhost",
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
     }
     # if os.getenv("DB_MODE") == "prod"
     # else {

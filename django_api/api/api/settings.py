e62612d06189dd9e9e76c2fe88edae1fde9db8d8
IronCow <uangeji@gmail.com>
2023-12-26 19:49:50 +0200

    added nginx to the app

--- django_api/api/api/settings.py
+++ django_api/api/api/settings.py
@@ -35,10 +35,11 @@ SECRET_KEY = "django-insecure-xi8za@kq+q)#tek@7d$j*hq!_e)a+r5(4w94&e9x5gh0yfibrm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",") or [
     "0.0.0.0",
     "127.0.0.1",
-    "localhost",
+    "localhost", 
 ]
 
 # Application definition
--- nginx_script.sh
+++ nginx_script.sh
@@ -3,7 +3,9 @@
 echo "Starting Nginx with API_PORT=$API_PORT"
 
 # Replace the API_PORT placeholder
-sed -i "s/\${API_PORT}/$API_PORT/g" /etc/nginx/nginx.conf
+sed -i "s/\${PORT_API_HOST}/$PORT_API_HOST/g" /etc/nginx/nginx.conf
+sed -i "s/\${PORT_FRONTEND}/$PORT_FRONTEND/g" /etc/nginx/nginx.conf
+sed -i "s/\${HOSTNAME_BACKEND}/$HOSTNAME_BACKEND/g" /etc/nginx/nginx.conf
 sed -i "s/\${SSL}/$SSL/g" /etc/nginx/nginx.conf
 
 # Start Nginx

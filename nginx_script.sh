3c0ba06d5b4ef2f304669f8260e98bec9687c087
IronCow <uangeji@gmail.com>
2023-12-27 18:54:54 +0200

    fix ports for nginx 2

--- nginx_script.sh
+++ nginx_script.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-echo "Starting Nginx with API_PORT=$PORT_API_HOST"
+echo "Starting Nginx with PORT_ENTRY_NGINX=$PORT_ENTRY_NGINX"
+echo "Starting Nginx with PORT_API_CONTAINER=$PORT_API_CONTAINER"
+echo "Starting Nginx with HOSTNAME_BACKEND=$HOSTNAME_BACKEND"
+echo "Starting Nginx with HOSTNAME_FRONTEND=$HOSTNAME_FRONTEND"
 
 # Replace the API_PORT placeholder
-sed -i "s/\${PORT_API_HOST}/$PORT_API_HOST/g" /etc/nginx/nginx.conf
+sed -i "s/\${PORT_API_CONTAINER}/$PORT_API_CONTAINER/g" /etc/nginx/nginx.conf
 sed -i "s/\${PORT_FRONTEND}/$PORT_FRONTEND/g" /etc/nginx/nginx.conf
 sed -i "s/\${HOSTNAME_BACKEND}/$HOSTNAME_BACKEND/g" /etc/nginx/nginx.conf
 sed -i "s/\${HOSTNAME_FRONTEND}/$HOSTNAME_FRONTEND/g" /etc/nginx/nginx.conf

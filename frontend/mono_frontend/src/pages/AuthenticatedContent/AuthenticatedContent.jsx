de67570b106e10ac7b840777c6b06449bde54b41
Dmitro Foliushnial <drunkluffy@gmail.com>
2024-02-04 20:11:15 +0200

    add cards page

---
+++ frontend/mono_frontend/src/pages/AuthenticatedContent/AuthenticatedContent.jsx
@@ -0,0 +1,44 @@
+import { Link, Outlet, useLoaderData } from 'react-router-dom'
+import '../AuthenticatedContent/AuthenticatedContent.css'
+
+export default function AuthenticatedContent() {
+	const token = useLoaderData()
+	console.log('AuthenticatedContent ->', token)
+
+	return (
+		<>
+			<header className='header'>
+				<div className='header__box'>
+					<nav className='header__nav'>
+						<ul className='header__list'>
+							<li className='header__item'>
+								<Link className='header__link' to='/cards'>
+									Cards
+								</Link>
+							</li>
+							<li className='header__item'>
+								<Link className='header__link' to='#'>
+									Jars
+								</Link>
+							</li>
+							<li className='header__item'>
+								<Link className='header__link' to='#'>
+									Transactions
+								</Link>
+							</li>
+						</ul>
+						<div className='header__logout'>
+							<Link to='/logout' role='button' className='header__link'>
+								Logout
+							</Link>
+						</div>
+					</nav>
+				</div>
+			</header>
+			<main>
+				{/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+				<Outlet />
+			</main>
+		</>
+	)
+}
---
+++ frontend/mono_frontend/src/pages/CardsList/CardsList.jsx
@@ -0,0 +1,77 @@
+import { useRouteLoaderData } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { BACKEND_URL } from '../../config/envs'
+import './CardsList.css'
+
+export const getCards = async function (token) {
+	const endpoint = `${BACKEND_URL}/monobank/monocards`
+
+	const response = await fetch(endpoint, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${token}`
+		}
+	})
+	const data = await response.json()
+
+	if (!response.ok) {
+		return null
+	}
+	return data
+}
+
+export const CardsList = () => {
+	const token = useRouteLoaderData('token')
+	const [cardData, setCardData] = useState(null)
+
+	useEffect(() => {
+		const fetchData = async function () {
+			try {
+				const access = await getCards(token)
+				setCardData(access)
+			} catch (error) {
+				console.error('Error fetching card data:', error)
+			}
+		}
+
+		let ignore = fetchData()
+	}, [token])
+
+	return (
+		<>
+			{cardData && (
+				<div>
+					<table className='table'>
+						<thead className='table__head'>
+							<tr key={1} className='table__row'>
+								<th className='table__title'>№</th>
+								<th className='table__title'>Type</th>
+								<th className='table__title'>Balance</th>
+								<th className='table__title'>Currency</th>
+								<th className='table__title'>Credit limit</th>
+							</tr>
+						</thead>
+						<tbody className='table__body'>
+							{cardData.map(
+								({ id, type, currency, balance, credit_limit }, index) => (
+									<tr className='table__body_tr' key={index}>
+										<td className='table__description'>{index + 1}</td>
+										<td className='table__description'>{type}</td>
+										<td className='table__description'>
+											{(balance / 100).toFixed(2)}
+										</td>
+										<td className='table__description'>{currency.name}</td>
+										<td className='table__description'>
+											{credit_limit.toFixed(2)}
+										</td>
+									</tr>
+								)
+							)}
+						</tbody>
+					</table>
+				</div>
+			)}
+		</>
+	)
+}

10f1b5640c1a488353340ad0569c3853d17329b1
Dmitro Foliushnial <drunkluffy@gmail.com>
2024-02-04 20:11:15 +0200

    add cards page

--- frontend/mono_frontend/src/App.js
+++ frontend/mono_frontend/src/App.js
@@ -1,45 +1,53 @@
-import React from 'react';
-import './App.css';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import LoginPage, {action as loginAction} from "./pages/LoginPage";
-import {action as logoutAction} from "./pages/Logout";
-import AuthenticatedContent from "./pages/AuthenticatedContent";
-import Root from "./pages/Root";
-import {checkAuthLoader} from "./utils/auth";
+import React from 'react'
+import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LoginPage, { action as loginAction } from './pages/LoginPage'
+import { action as logoutAction } from './pages/Logout'
+import AuthenticatedContent from './pages/AuthenticatedContent/AuthenticatedContent'
+import Root from './pages/Root'
+import { checkAuthLoader } from './utils/auth'
+import { CardsList } from './pages/CardsList/CardsList'
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Root/>,
-        children: [
-            {
-                path: 'login',
-                element: <LoginPage/>,
-                action: loginAction,
-                errorElement: <div>Login failed, and it is worse, that we all thought :( Please contact admin to get
-                    some help</div>,
-                // children: []
-            },
-            {
-                path: 'logout',
-                loader: logoutAction,
-            },
-            {
-                path: '',
-                element: <AuthenticatedContent/>,
-                loader: checkAuthLoader,
-
-
-                // errorElement: <ErrorPage />,
-                // children: []
-            },
-        ]
-    },
-
+	{
+		path: '/',
+		element: <Root />,
+		children: [
+			{
+				path: 'login',
+				element: <LoginPage />,
+				action: loginAction,
+				errorElement: (
+					<div>
+						Login failed, and it is worse, that we all thought :( Please contact
+						admin to get some help
+					</div>
+				)
+				// children: []
+			},
+			{
+				path: 'logout',
+				loader: logoutAction
+			},
+			{
+				path: '',
+				element: <AuthenticatedContent />,
+				loader: checkAuthLoader,
+				id: 'token',
+				// errorElement: <ErrorPage />,
+				children: [
+					{
+						path: 'cards',
+						element: <CardsList />
+					}
+				]
+			}
+		]
+	}
 ])
 
 function App() {
-    return <RouterProvider router={router}/>;
+	return <RouterProvider router={router} />
 }
 
-export default App;
+export default App
